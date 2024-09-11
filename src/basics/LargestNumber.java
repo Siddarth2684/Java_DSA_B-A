2054e11904cecc594a01d309b9082b2ff3816b8b
Siddarth2684 <siddarthilk@gmail.com>
2024-09-11 16:17:26 +0530

    just added a comments

--- src/basics/LargestNumber.java
+++ src/basics/LargestNumber.java
@@ -16,5 +16,10 @@ public class LargestNumber {
     public static int largestnum(int n1, int n2, int n3){
         //using nested Ternery Operator
         return (n1>n2 || n1>n3)? n1 :(Math.max(n2, n3));
+
+
+//        using a inbuit function Math.max
+//        int max=Math.max(Math.max(n1,n2),n3);
+//        return max;
     }
 }
