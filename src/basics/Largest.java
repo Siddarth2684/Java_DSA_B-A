97a8f1b0511d8eb3c838b9daf363506ca922f05e
Siddarth2684 <siddarthilk@gmail.com>
2024-09-11 15:43:11 +0530

    First commit

---
+++ src/basics/Largest.java
@@ -0,0 +1,17 @@
+package basics;
+
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        System.out.println("The Largest Number is  "+largestnum(a,b,c));
+    }
+    public static int largestnum(int n1, int n2, int n3){
+        return (n1>n2 || n1>n3)? n1 :(Math.max(n2, n3));
+    }
+}
---
+++ src/basics/Main.java
@@ -0,0 +1,17 @@
+package basics;
+
+import java.awt.desktop.SystemEventListener;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
