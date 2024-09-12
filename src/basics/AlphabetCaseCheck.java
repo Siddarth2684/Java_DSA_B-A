cb1424b2c11679a547a11e727a26223d809b0957
Siddarth2684 <siddarthilk@gmail.com>
2024-09-12 11:08:59 +0530

    Fibonnaci series Program

--- src/basics/AlphabetCaseCheck.java
+++ src/basics/AlphabetCaseCheck.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class AlphabetCaseCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter a Character ");
         char ch =input.next().trim().charAt(0);
         if (ch == 'a' && ch == 'z')
         {
---
+++ src/basics/FibonnaciSeries.java
@@ -0,0 +1,25 @@
+package basics;
+
+import java.util.Scanner;
+
+public class FibonnaciSeries {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a Number ");
+        int n = input.nextInt();
+        int first = 0;
+        int second = 1;
+        System.out.print(+first+ " " +second);
+        int third;
+        int count=2;
+        while(count<n)
+        {
+            third = first + second;
+            System.out.print(" " + third);
+            first=second;
+            second=third;
+            count++;
+        }
+
+    }
+}
